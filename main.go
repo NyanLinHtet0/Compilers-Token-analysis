a91d9b9d07f2f664f66967b7928c3dff1097383b
Your Name <harrishtet.nyanlinhtet@gmail.com>
2025-10-29 15:58:53 -0700

    setted up code for parsing python code

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello, World!")
+	file, err := os.Open("main.py")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Println(line)
+	}
 }
